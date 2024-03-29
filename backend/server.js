28bcd438a18e52ae4db4da71a16f5134d5a01743
Sakshi Bhokare <bhokaresakshi93@gmail.com>
2024-03-29 22:29:28 +0530

    first part backend complete

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,70 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
-import { connect } from "mongoose";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
-const app = express();
+import { app, server } from "./socket/socket.js";
+
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
 
 dotenv.config();
 
-app.use(express.json()); //to parse the incoming request with JSON payloads (from req.body)
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-
-// app.get("/", (re, res)=>{
-//     //root route http://localhost:5000/
-//     res.send("Hello World!");
-// })
-
-
-
-app.listen(PORT, ()=> {
-    connectToMongoDB();
-    console.log(`Server Running on port ${PORT}`)
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
+
+server.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
+});
+
+
+
+// import express from "express";
+// import dotenv from "dotenv";
+// import authRoutes from "./routes/auth.routes.js";
+// import { connect } from "mongoose";
+// import cookieParser from "cookie-parser";
+// import messageRoutes from "./routes/message.routes.js"
+// import connectToMongoDB from "./db/connectToMongoDB.js";
+// const app = express();
+// const PORT = process.env.PORT || 8000;
+
+
+// dotenv.config();
+
+// app.use(express.json()); //to parse the incoming request with JSON payloads (from req.body)
+// app.use(cookieParser());
+
+// app.use("/api/auth", authRoutes)
+// app.use("/api/message", messageRoutes)
+
+
+// // app.get("/", (re, res)=>{
+// //     //root route http://localhost:5000/
+// //     res.send("Hello World!");
+// // })
+
+
+
+// app.listen(PORT, ()=> {
+//     connectToMongoDB();
+//     console.log(`Server Running on port ${PORT}`)
+// });
 
